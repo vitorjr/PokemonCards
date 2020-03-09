cbc4217d5d864eaf22d2152fe844c6a84b75eca2
Joao Victor de A. Monteiro <joao.monteiro@fitbank.com.br>
2020-03-09 11:33:44 -0300

    Iniciando projeto PokemonCards

---
+++ PokemonCards/Entities/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonCards.Entities
+{
+    class Card
+    {
+        public string Name { get; set; }
+        public string Numeracao { get; set; }
+        public string Expansao { get; set; }
+        public string Image { get; set; }
+
+        public Card() { }
+
+        public Card(string name, string numeracao, string expansao, string image)
+        {
+            Name = name;
+            Numeracao = numeracao;
+            Expansao = expansao;
+            Image = image;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ PokemonCards/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("1 - Acesse o site https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/");
+            Console.WriteLine("2 - Realize uma pesquisa sem preencher nenhum campo(Clicando em Search)");
+
+            Console.WriteLine("3 - Informe abaixo a quantidade de páginas:");
+            Console.WriteLine("");*/
+        }
+    }
+}
