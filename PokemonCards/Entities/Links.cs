efda22e671d3242bf9611f20caf4dcf311fe356e
Joao Victor de A. Monteiro <joao.monteiro@fitbank.com.br>
2020-03-09 15:31:21 -0300

    Criando Entities e adicionando atributos as classes

--- PokemonCards/Entities/Links.cs
+++ PokemonCards/Entities/Links.cs
@@ -8,6 +8,5 @@ namespace PokemonCards.Entities
 {
     class Links
     {
-
     }
 }
--- PokemonCards/Program.cs
+++ PokemonCards/Program.cs
@@ -10,11 +10,18 @@ namespace PokemonCards
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("1 - Acesse o site https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/");
-            Console.WriteLine("2 - Realize uma pesquisa sem preencher nenhum campo(Clicando em Search)");
-
-            Console.WriteLine("3 - Informe abaixo a quantidade de páginas:");
-            Console.WriteLine("");*/
+            
+            try{
+                Console.WriteLine("1 - Acesse o site https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/");
+                Console.WriteLine("2 - Realize uma pesquisa sem preencher nenhum campo(Clicando em Search)");                
+                Console.WriteLine("3 - Informe abaixo a quantidade de páginas:");
+                Console.WriteLine("");
+                int numero_paginas = int.Parse(Console.ReadLine());
+            }
+            catch (Exception f){
+                Console.WriteLine(f.Message);
+            }
+            
         }
     }
 }
